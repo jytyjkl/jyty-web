9ab7e2a97b24f7f3a7ea32be96e937000a382a54
Juho Järvi <juho.jarvi@solteq.com>
2025-11-23 18:31:00 +0100

    feat: added emails

--- pages/hallitus.js
+++ pages/hallitus.js
@@ -6,46 +6,55 @@ const members = [
     name: "Juho Järvi",
     title: "Puheenjohtaja",
     image: "hallitus/juho.jpg",
+    email: "pj@jytyjkl.fi",
   },
   {
     name: "Eemil Hukkanen",
     title: "Varapuheenjohtaja",
     image: "hallitus/eemil.jpg",
+    email: "pj@jytyjkl.fi,
   },
   {
     name: "Ilmo Kurki",
     title: "Sihteeri",
     image: "hallitus/ilmo.jpg",
+    email: "ilmo.kurki@jytyjkl.fi",
   },
   {
     name: "Nuutti Rantanen",
     title: "Rahastonhoitaja",
     image: "hallitus/nuutti.jpg",
+    email: "nuutti.rantanen@jytyjkl.fi",
   },
   {
     name: "Kasper Pelkonen",
     title: "Kiltavastaava",
     image: "hallitus/kassu.jpg",
+    email: "kasper.pelkonen@jytyjkl.fi",
   },
   {
     name: "Lassi Laitinen",
     title: "Teekkarikulttuurivastaava",
     image: "hallitus/lassi.jpg",
+    email: "lassi.laitinen@jytyjkl.fi",
   },
   {
     name: "Atte Heikkinen",
     title: "Viestintävastaava",
     image: "hallitus/atte.jpg",
+    email: "atte.heikkinen@jytyjkl.fi",
   },
   {
     name: "Juho Kallijärvi",
     title: "Mediavastaava",
     image: "hallitus_placeholder.png",
+    email: "juho.kallijarvi@jytyjkl.fi",
   },
   {
     name: "Theodore Veistos",
     title: "Somevastaava",
     image: "hallitus_placeholder.png",
+    email: "theodore.veistos@jytyjkl.fi",
   },
 ];
 
@@ -60,11 +69,12 @@ export default function Hallitus() {
               <img src={`/${member.image}`} alt={member.name} />
               <h2>{member.name}</h2>
               <p>{member.title}</p>
+              <a href={`mailto:${member.email}`}>{member.email}</a>
             </div>
           ))}
         </div>
         <div className={styles["hallitus-contact"]}>
-          <a href="mailto:jyvaskylanteekkarit@gmail.com">Ota yhteyttä hallitukseen</a>
+          <a href="mailto:hallitus@jytyjkl.fi">Ota yhteyttä hallitukseen</a>
         </div>
       </div>
     </Layout>
