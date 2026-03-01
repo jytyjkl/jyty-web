a68c751006cfe1733f6e67cfe99fc394e4029fae
jumakall <57396294+jumakall@users.noreply.github.com>
2026-03-01 04:25:06 +0200

    Update dependencies

--- pages/kuulumiset/[slug].js
+++ pages/kuulumiset/[slug].js
@@ -2,15 +2,13 @@ import Layout from "../../components/Layout";
 import news from "../../data/kuulumiset.json";
 import styles from "../../styles/kuulumiset.module.css";
 
-export const runtime = "experimental-edge";
-
 export async function getStaticPaths() {
-  const paths = news.map(n => ({ params: { slug: n.slug } }));
+  const paths = news.map((n) => ({ params: { slug: n.slug } }));
   return { paths, fallback: false };
 }
 
 export async function getStaticProps({ params }) {
-  const item = news.find(n => n.slug === params.slug);
+  const item = news.find((n) => n.slug === params.slug);
   if (!item) return { notFound: true };
   return { props: { news: item } };
 }
@@ -25,8 +23,8 @@ export default function UutisPage({ news }) {
             {new Date(news.date).toLocaleDateString("fi-FI")}
           </div>
           {news.image && (
-            <img 
-              src={`/${news.image}`} 
+            <img
+              src={`/${news.image}`}
               alt={news.title}
               className={styles["news-hero-image"]}
             />
